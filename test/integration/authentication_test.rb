e2f4b1f4b7f7791f6f1031156c307b7ef9fea91a
Caillou <igorlacroute@gmail.com>
2017-09-08 16:26:37 +0000

    suspend integration tests for google login, must make custom user validation

--- test/integration/authentication_test.rb
+++ test/integration/authentication_test.rb
@@ -21,6 +21,19 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
     assert_equal user_count + 1, User.count
   end
   
+  # test "sign up with google makes a redirection to their login service" do
+  #   get user_google_oauth2_omniauth_authorize_path
+  #   assert_response :redirect
+  #   assert_match(/https:\/\/accounts\.google\.com\/o\/oauth2\/auth/, @response.redirect_url)
+  # end
+  
+  # test "omniauth callback controller should create user" do
+  #   get user_google_oauth2_omniauth_callback_path
+  #   assert_response :redirect
+  # end
+  
+  
+  
   test "can see the sign in page" do
     get "/users/sign_in"
     assert_select "h2", "Log in"
@@ -35,5 +48,6 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
     }
     assert_response :success
   end
+  
   
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "create a user from omniauth params" do
+     access_token = OpenStruct.new(
+        info: OpenStruct.new(email: "myname@test.com", name: "My Name")
+      )
+     user = User.from_omniauth(access_token)
+     assert_not_nil user
+   end
 end
