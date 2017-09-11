4055cf47f7fcdcbda3eae4a70cbfac3fc36e2b46
Caillou <igorlacroute@gmail.com>
2017-09-11 11:03:46 +0000

    test the get_account

--- app/models/youtube_api.rb
+++ app/models/youtube_api.rb
@@ -11,25 +11,29 @@ class YoutubeApi
        Yt::Video.new id: id
     end
     
+    def get_account(google_token)
+        Yt::Account.new refresh_token: google_token
+    end
+    
     private
         
         def initialize_secret_keys
             if(API_KEY)
                 Yt.configuration.api_key = API_KEY 
             else
-                raise LoadError, "Can't find environment variable 'YOUTUBE_API_KEY'"
+                raise LoadError, "Can't find environment variable 'GOOGLE_API_KEY'"
             end
             
             if(OAUTH_CLIENT_ID)
                 Yt.configuration.client_id = OAUTH_CLIENT_ID
             else
-                raise LoadError, "Can't find environment variable 'YOUTUBE_OAUTH_CLIENT_ID'"
+                raise LoadError, "Can't find environment variable 'GOOGLE_CLIENT_ID'"
             end
             
             if(OAUTH_CLIENT_SECRET)
                 Yt.configuration.client_secret = OAUTH_CLIENT_SECRET
             else 
-                raise LoadError, "Can't find environment variable 'YOUTUBE_OAUTH_CLIENT_SECRET'"
+                raise LoadError, "Can't find environment variable 'GOOGLE_CLIENT_SECRET'"
             end
         end
 end
--- test/models/youtube_api_test.rb
+++ test/models/youtube_api_test.rb
@@ -15,6 +15,12 @@ class YoutubeApiTest < ActiveSupport::TestCase
     assert_equal video.title, 'Rick Astley - Never Gonna Give You Up'
   end
   
+  test "check the get_account" do
+    refresh_token = "1/neJAsmsyiNlDkj6sG7Df2c4afUHNkKdV5IQ9uXFmv6Q"
+    account = @youtube_api.get_account(refresh_token)
+    assert_equal "JeSuisUnCaill0u", account.name
+  end
+  
   
   
 end
