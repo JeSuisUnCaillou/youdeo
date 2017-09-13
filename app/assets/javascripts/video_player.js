1e031ab89bf44065b4f329d6cd4d99f1af1a4159
Caillou <igorlacroute@gmail.com>
2017-09-13 13:22:27 +0000

    clicking a video now reset the youtube player

--- app/assets/javascripts/video_player.js
+++ app/assets/javascripts/video_player.js
@@ -1,5 +1,25 @@
 
 //Video player
 $(document).ready(function() {
-    alert("coucou");
+    var video_player = $('#video_player')
+    var video_cards = $('.video_card')
+    
+    video_cards.each(function(index, card){
+        var video_card = $(card)
+        var uid = get_video_uid(video_card)
+        video_card.click(function(e){
+            e.preventDefault()
+            reset_video_player(uid)
+            return false
+        })
+    })
+    
+    //Extracts the video uid from the html id of the video_card
+    function get_video_uid(video_card){
+        return video_card.attr('attr-uid')
+    }
+    
+    function reset_video_player(video_uid){
+        video_player.attr('src', "https://www.youtube.com/embed/" + video_uid)
+    }
 });
