75e31bf81f83c609584eda348edd925c2370dc51
Caillou <igorlacroute@gmail.com>
2017-09-13 17:49:14 +0000

    add list of other tags to the tag choice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
             }.to_h
         end
         
+        @other_tags_with_count = Tag.all_with_channels_count.except(*@tags.keys)
+        
         @elapsed_time = ((DateTime.now - init_time) * 24 * 60 * 60).to_i
     end
     
